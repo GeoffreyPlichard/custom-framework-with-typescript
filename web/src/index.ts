2c45a6d63822c7865572de06a3851930fed6b4c2
Geoffrey <geoffreyplichard@gmail.com>
2024-05-14 18:17:59 +0200

    implement Model in User static method

--- web/src/index.ts
+++ web/src/index.ts
@@ -1,6 +1,6 @@
 import { User } from "./models/User";
 
-const user = new User({ id: '16de', name: 'coucou', age: 43 });
+const user = User.buildUser({ id: '16de', name: 'coucou', age: 43 });
 
 
 user.on('save', () => {
